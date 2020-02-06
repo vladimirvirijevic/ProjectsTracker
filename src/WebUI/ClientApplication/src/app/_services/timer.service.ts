092d150a2146ab65fae06ff9f909877006fa064f
vladimirvir1 <vladanfs96@gmail.com>
2020-02-06 18:08:50 +0100

    Create timer with start/stop functions on frontend

---
+++ src/WebUI/ClientApplication/src/app/_services/timer.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimerService {
+
+  constructor() { }
+
+  startTimer(time) {
+    localStorage.setItem('startingTime', time);
+    localStorage.setItem('timerIsRunning', 'TRUE');
+  }
+
+  isTimerRunning(): boolean {
+    if (localStorage.getItem('timerIsRunning') === 'TRUE') {
+      return true;
+    }
+    
+    return false;
+  }
+
+  getStartingTime(): number {
+    if (localStorage.getItem('startingTime')) {
+      return Number(localStorage.getItem('startingTime'));
+    }
+    
+    return -100;
+  }
+
+  stopTimer() {
+    localStorage.removeItem('startingTime');
+    localStorage.setItem('timerIsRunning', 'FALSE');
+  }
+}
--- src/WebUI/ClientApplication/src/app/timer/timer.component.ts
+++ src/WebUI/ClientApplication/src/app/timer/timer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TimerService, ProjectService, AuthenticationService } from '../_services';
+import { Project } from '../_models';
 
 @Component({
   selector: 'app-timer',
@@ -6,10 +8,67 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class TimerComponent implements OnInit {
+  time: number;
+  startTime: number;
+  timer: any;
+  selectedProjectName: string;
+  projects: Project[];
+  timerIsRunning = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private timerService: TimerService,
+    private projectService: ProjectService,
+    private authService: AuthenticationService
+  ) {
+    if (this.timerService.isTimerRunning()) {
+      this.startTime = timerService.getStartingTime();
+      this.displayTime();
+    }
   }
 
+  ngOnInit() {
+    this.timerIsRunning = this.timerService.isTimerRunning();
+    this.getProjects();
+  }
+
+  displayTime() {
+    this.timer = setInterval(() => {
+      this.time = Date.now() - this.startTime;
+      console.log(this.time);
+    }, 1000);
+  }
+    
+  startTimer() {
+    if (this.timerService.isTimerRunning()) {
+      return;
+    }
+    this.timerIsRunning = true;
+    this.startTime = Date.now();
+    this.timerService.startTimer(this.startTime);
+    this.displayTime();
+  }
+
+  stopTimer() {
+    this.timerIsRunning = false;
+    this.time = 0;
+    this.timerService.stopTimer();
+    clearInterval(this.timer);
+  }
+
+  getProjects() {
+    this.projectService.getAll(this.authService.currentUserValue.username)
+      .subscribe(
+        data => {
+          this.projects = data;
+          // set default selected project name to first project
+          if (this.projects) {
+            this.selectedProjectName = this.projects[0].name;
+          }
+        }
+      );
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
+  }
 }
